c5ca0f0e801fc838a6f9f71cc3dd157a146d4d1c
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-09 18:34:02 +0300

    fixed conditional check

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(97, 123):
-        if c == chr(i):
+        if ord(c) > 96 and ord(c) < 123:
             return (True)
-    return (False)
+        else:
+            return (False)
