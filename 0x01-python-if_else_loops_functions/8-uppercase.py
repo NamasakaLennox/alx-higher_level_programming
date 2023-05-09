50c34d1cdf5ae3c9c200001410e2b439251845f6
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-09 15:04:28 +0300

    prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for char in str:
+        check = ord(char)
+        if (check > 96 and check < 123):
+            check -= 32
+        print("{}".format(chr(check)), end="")
+    print()
