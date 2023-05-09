b498c22dfdfeaf168bc48e5318c9581cba2581d9
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-09 12:36:01 +0300

    corrected logic operation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,20 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     check = number * -1
+    if (check % 10) > 5:
+        print(f"Last digit of {number:d} is {-(check % 10)}", end=" ")
+        print("and is greater than 5", end="\n")
+    elif (check % 10) == 0:
+        print(f"Last digit of {number:d} is {check % 10} and is 0")
+    else:
+        print(f"Last digit of {number:d} is {-(check % 10)}", end=" ")
+        print("and is less than 6 and not 0")
 else:
-    check = number
-if (check % 10) > 5:
-    print(f"Last digit of {number:d} is {check % 10} and is greater than 5")
-elif (check % 10) == 0:
-    print(f"Last digit of {number:d} is {check % 10} and is 0")
-else:
-    print(f"Last digit of {number:d} is {check % 10} and is less than 6 and not 0")
+    if (number % 10) > 5:
+        print(f"Last digit of {number:d} is {number % 10}", end=" ")
+        print("and is greater than 5")
+    elif (number % 10) == 0:
+        print(f"Last digit of {number:d} is {number % 10} and is 0")
+    else:
+        print(f"Last digit of {number:d} is {number % 10}", end=" ")
+        print("and is less than 6 and not 0")
