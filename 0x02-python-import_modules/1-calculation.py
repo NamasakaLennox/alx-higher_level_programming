5fd4cfce057f6f37aff4f2658235aba013991500
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-11 11:26:53 +0300

    calls a function that performs basic operations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+a = 10
+b = 5
+print(f"{a:d} + {b:d} = {calc.add(a, b):d}")
+print(f"{a:d} - {b:d} = {calc.sub(a, b):d}")
+print(f"{a:d} * {b:d} = {calc.mul(a, b):d}")
+print(f"{a:d} / {b:d} = {calc.div(a, b):d}")
