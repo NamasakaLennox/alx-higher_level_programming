e4080fb9d7ea8a78c11adc401c13f9c97e74194b
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-12 14:48:26 +0300

    prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for elem in range(len(matrix)):  # iterate through each row
+        elem_length = len(matrix[elem])  # length of each column
+
+        for val in range(elem_length):  # iterate through each column
+            num = matrix[elem][val]  # value contained in each row x column
+
+            if (val == elem_length - 1):
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end=" ")  # separate with space
+
+            print(end="$\n")
