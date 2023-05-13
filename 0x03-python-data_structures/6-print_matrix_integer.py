c51aec4b822e135103778742856b829722adadd4
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-13 14:53:44 +0300

    fixed syntax error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for elem in range(len(matrix)):  # iterate through each row
-        for val in range(len(matrix[elem]):  # iterate through each column
+        for val in range(len(matrix[elem])):  # iterate through each column
             if (val != 0):
                 print(end=" ")
             else:
                 print("{:d}".format(matrix[elem][val]), end="")
-            print()
+        print()
