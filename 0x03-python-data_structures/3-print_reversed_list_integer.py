f4694550a28cc369a02ed49658ea559269b1b4ba
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-13 14:36:32 +0300

    fixed condition for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    index = len(my_list) - 1
-    for num in range(index, -1, -1):
-        print("{:d}".format(my_list[num]))
+    if not my_list:
+        pass
+    else:
+        index = len(my_list) - 1
+        for num in range(index, -1, -1):
+            print("{:d}".format(my_list[num]))
