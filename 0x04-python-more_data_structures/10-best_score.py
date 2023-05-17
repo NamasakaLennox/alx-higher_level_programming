fd190b327336aae9a4daf385882f119c3cdf88bc
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-17 14:03:50 +0300

    returns the largest value correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,6 +7,8 @@ def best_score(a_dictionary):
         return (None)
     max_val = a_dictionary[list(a_dictionary.keys())[0]]
     for key in a_dictionary:
+        if not a_dictionary[key]:
+            continue
         if a_dictionary[key] > max_val:
             max_key = key
     return (max_key)
