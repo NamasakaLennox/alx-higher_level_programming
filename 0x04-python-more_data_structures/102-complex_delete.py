986b87babea12cafe91ab1b5717bdf3d0c915d3a
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-17 18:22:28 +0300

    deletes a value from list

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for keys in list(a_dictionary.keys()):
+        if a_dictionary[keys] == value:
+            del a_dictionary[keys]
+    return (a_dictionary)
