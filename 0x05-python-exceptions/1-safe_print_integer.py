3175e2ed902ac221499a48580fe42330bc9b49a2
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-29 12:14:27 +0300

    made universal except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-    except ValueError, TypeError:
+    except:
         return (False)
     else:
         return (True)
