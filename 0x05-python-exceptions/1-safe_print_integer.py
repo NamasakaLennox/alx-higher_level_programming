c280b937d3fa8a773171c508d4c23b12dba10893
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-29 10:41:21 +0300

    prints an integer safely

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    prints an integer value
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return (False)
+    else:
+        return (True)
