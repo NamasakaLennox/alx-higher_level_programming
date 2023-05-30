79867c6cc9770b1bde0135e25f1dc55ad8654b76
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-30 20:01:05 +0300

    defines a square with a setter function

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A python module defining a class Square with private attributes
+"""
+
+
+class Square:
+    """
+    A class Square defining an attribute size
+    """
+    def __init__(self, size=0):
+        """
+        initializes an instance attribute with given value
+        or zero if no value is given
+        """
+        self.__size = size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter function to set the value of private attribute
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        finds the area of a square
+        """
+        return (self.__size ** 2)
