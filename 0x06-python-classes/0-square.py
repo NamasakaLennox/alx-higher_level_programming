a89eaf06b84532b26af2b4c1f2fe14c2683a0f8a
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-30 18:55:31 +0300

    module doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+python module that defines a class square.
+"""
 class Square:
-    """Class Square that defines a square.
+    """
+    Class Square that defines a square.
     """
     pass
