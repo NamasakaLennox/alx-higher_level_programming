b89abc87dda5933a13bea90ffd1d0d73294f66f7
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-30 19:08:52 +0300

    a class that defines a private attribute class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module that defines a class Square
+"""
+
+
+class Square:
+    """
+    A class Square that defines a private instance attribute size
+    """
+    def __init__(self, size):
+        """
+        initializes the size attribute
+        """
+        self.__size = size
