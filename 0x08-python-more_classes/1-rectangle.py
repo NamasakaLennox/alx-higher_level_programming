3cc6402a886878107339e42e669f61cde8541d20
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-05 16:51:09 +0300

    edit: redefined properties to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         """
         Initializes the private attributes width and height
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
