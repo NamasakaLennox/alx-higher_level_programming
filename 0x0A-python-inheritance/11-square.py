7eb2f94bfd7373b4433619c5794e7858a2a273d4
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-12 23:35:11 +0300

    implements the magic method __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A module containing a class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class that inherits properties from the Rectangle class
+    """
+    def __init__(self, size):
+        """
+        Initializes the square class with its properties
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """
+        gets the area of the square
+        """
+        return super().area()
+
+    def __str__(self):
+        """
+        magic method that returns a formated string
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
