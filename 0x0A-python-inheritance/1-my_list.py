dec9bef62767c606e868b4b5512f75efc4c3a5a5
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-12 20:21:03 +0300

    last test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """
-Module that prints a list in a sorted order
+contains the MyList class
 """
 
 
 class MyList(list):
-    """
-    Inherits a list object
-    prints a list in a sorted order
-    Args:
-        list: the list object
-    """
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """
-        prints the list in a sorted way
-        """
+        """Print a list in sorted ascending order."""
         print(sorted(self))
