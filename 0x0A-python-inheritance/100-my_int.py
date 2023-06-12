5cf48d59b4ecc24cd6ba0768570b11deb10c8367
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-12 23:53:09 +0300

    invers '==' and '!=' operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyhton3
+"""
+module containing a class that inverts '==' and '!='
+"""
+
+
+class MyInt(int):
+    """
+    A class containing methods that invert equal to and not equal to operators
+    """
+    def __eq__(self, other):
+        """
+        equal to magic method - inverts to not equal to
+        """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """
+        not equal to magic method - inverts to equal to
+        """
+        return int.__eq__(self, other)
