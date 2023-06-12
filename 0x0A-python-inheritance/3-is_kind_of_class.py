5a767398f00fc215a61af3587d5f509f4ec12a9f
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-12 20:56:03 +0300

    checks if object is an instace of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains a function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if an object is an instance of, or if the object is an instance of
+    a class that inherited from
+    """
+    return (isinstance(obj, a_class))
