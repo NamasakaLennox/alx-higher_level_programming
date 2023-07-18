f786a2d9ab46b8a754a8ec5e12289cdb243dcf2e
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-18 20:50:16 +0300

    lists all records with a score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records with a score >= 10 in the table second_table
+-- lists and orders score in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
