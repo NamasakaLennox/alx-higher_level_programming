ef14c6e479d01270cc4928195c7b83f82313bb67
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-18 19:39:21 +0300

    creates a table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- A script that creates a table called first_table in the current database
+-- create a table called first_table
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
