77d578a6b921b80c59f3a97f84bacfd8949cd0d2
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-19 20:53:28 +0300

    edited it to use subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 -- lists all the cities of california that can be found in the database
 -- select the cities in California
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE states.name='California'
-ORDER BY cities.id ASC;
+SELECT id, name
+FROM cities
+WHERE state_id = ( -- get the id of California
+      SELECT id
+      FROM states
+      WHERE name = 'California')
+ORDER BY id ASC;
