a47b73aafad61eb1d8ba007ee6dd5d2372c2b7e2
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-19 19:43:55 +0300

    creates the table on your MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- a script that creates a table called force_name on your MySQL server
+-- create table
+CREATE TABLE IF NOT EXISTS force_name
+(id INT,
+name VARCHAR(256) NOT NULL); -- name cannot be null
