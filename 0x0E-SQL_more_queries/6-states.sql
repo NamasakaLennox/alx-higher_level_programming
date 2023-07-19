e476e420cb376ddfe24586bb76e10cebeff113cc
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-19 20:16:52 +0300

    creates a table with primary key and autogenerated id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database hbtn_0d_usa and  the table states
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa; -- Switch to database
+CREATE TABLE IF NOT EXISTS states
+(id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
