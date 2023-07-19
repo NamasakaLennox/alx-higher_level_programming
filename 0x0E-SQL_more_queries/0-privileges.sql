193b96379a99c37265d60a5c7e966a5194aca4ef
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-19 18:34:01 +0300

    lists all privilages for 2 files

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- a script that lists all privileges of he MySQL users
+-- lists privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
