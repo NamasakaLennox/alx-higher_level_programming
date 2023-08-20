1e34317579270c85439de6318c33e6882aa8267b
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-20 13:22:53 +0300

    added docs for the file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""lists all states from the database that match a passed argument"""
 
 import MySQLdb
 from sys import argv
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{:s}' ORDER BY states.id"
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id"
                 .format(argv[4]))
     rows = cur.fetchall()
 
