2e7ec59f4dd4030ee8325e67d4f99353f9098296
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-31 20:33:19 +0300

    added a silent transaction

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request with the contents of the file
-curl -X POST "$1"  -H "Content-Type: application/json" -d @"$2"
+curl -sX POST "$1"  -H "Content-Type: application/json" -d @"$2"
