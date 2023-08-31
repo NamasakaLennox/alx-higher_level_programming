7aa302232f32ca916f2882d61eac87806af399a4
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-31 16:17:01 +0300

    fixed it to take in an argument

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of a requests response
-curl -sI 0.0.0.0:5000 | grep -iF "Content-Length" | sed s/"Content-Length: "//
+curl -sI "$1" | grep -iF "Content-Length" | sed s/"Content-Length: "//
