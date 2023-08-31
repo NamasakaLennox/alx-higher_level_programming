7ce3efdc33e057937b7d28ba8d84f8c6a7be0473
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-31 16:57:43 +0300

    fixed argument arrangement

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a get request to the url and displays the body of a response
-# a header variable must be included
-curl -s "$1" -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "$1"
