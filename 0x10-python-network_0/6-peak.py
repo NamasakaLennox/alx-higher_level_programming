378bc12e76336b1db8a4d0298a666ac968cfa078
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-31 18:55:35 +0300

    discarded merge sort for efficiency issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,40 +4,6 @@ find the peak of a list
 """
 
 
-def merge_list(list_n):
-    """
-    sorts the lists using merge sort algorithm
-    """
-    if len(list_n) > 1:
-        mid = len(list_n) // 2  # find mid value - floor division
-        left = list_n[:mid]  # left array
-        right = list_n[mid:]  # right array
-        # split the array to the smallest possible array
-        merge_list(left)
-        merge_list(right)
-
-        # compare values of each side and sort the array
-        i = j = k = 0
-        while (i < len(left) and j < len(right)):
-            if left[i] < right[j]:
-                list_n[k] - left[i]
-                i += 1
-            else:
-                list_n[k] = right[j]
-                j += 1
-            k += 1
-        # fill any elements left
-        while i < len(left):
-            list_n[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            list_n[k] = right[j]
-            j += 1
-            k += 1
-
-
 def find_peak(list_of_integers):
     """
     finds the peak of a list
@@ -45,7 +11,29 @@ def find_peak(list_of_integers):
         list_of_integers - the list to check
     Return:
         the largest element
+    PS: merge sort takes O(log(n) so not worth sorting
     """
-    if type(list_of_integers) == list and len(list_of_integers) > 0:
-        merge_list(list_of_integers)
-        return list_of_integers[-1]
+
+    length = len(list_of_integers)
+    if length == 0:
+        return
+    elif length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return (list_of_integers[0] if list_of_integers[0] >=
+                list_of_integers[1] else list_of_integers[1])
+
+    # for lists greater than 2 elements
+    for i in range(length):
+        # check if second element is smaller than first element
+        if (i == 0 and list_of_integers[i + 1] <= list_of_integers[i]):
+            return list_of_integers[i]
+        # check if the items on the right and left of an element are both
+        # less than the current integer
+        elif (i > 0 and i < length - 1 and list_of_integers[i] >=
+              list_of_integers[i + 1] and list_of_integers[i] >=
+              list_of_integers[i - 1]):
+            return (list_of_integers[i])
+        # if the last item is larger than the previous item
+        elif (i == length - 1 and list_of_integers[i - 1] <= value):
+            return list_of_integers[i]
