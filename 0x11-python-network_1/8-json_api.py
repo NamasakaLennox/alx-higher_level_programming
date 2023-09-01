a05114f85e9d7a87f498c6660b1d4127d7543772
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-01 19:29:09 +0300

    requirement fail

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,24 +2,20 @@
 """
 requests from an api and searches for a letter
 """
-from sys import argv
+import sys
 import requests
 
-if __name__ = '__main__':
-    if len(argv) > 1:
-        q = argv[1]
-    else:
-        q = ""
 
-    url = 'http://0.0.0.0:5000/search_user'
-    params = {'q': q}
-    r = requests.post(url, data=params)
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        res = r.json()
-        if res:
-            print('[{}] {}'.format(res['id'], res['name']))
+        response = r.json()
+        if response == {}:
+            print("No result")
         else:
-            print('No result')
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
-        print('Not a valid JSON')
+        print("Not a valid JSON")
