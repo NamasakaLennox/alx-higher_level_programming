5257250cb35dc2a116bcbbe66623d032ea21d93b
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-01 21:32:15 +0300

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-prints the last 10 commits of a repository 'rails' by user 'rails'
-"""
-from sys import argv
+import sys
 import requests
 
-if __name__ == "__main__":
-    if len(argv) > 2:
-        user = argv[2]
-        repo = argv[1]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
 
-        r = requests.get(url)
-        commits = r.json()
-        try:
-            for i in range(10):
-                print("{}: {}".format(
-                    commits[i].get("sha"),
-                    commits[i].get("commit").get("author").get("name")))
-        except IndexError:
-            pass
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
