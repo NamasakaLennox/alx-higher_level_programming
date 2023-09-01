7fd079dee1440fa3d1a70e65ca307c7571711579
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-01 21:31:22 +0300

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,18 @@ prints the last 10 commits of a repository 'rails' by user 'rails'
 from sys import argv
 import requests
 
-if len(argv) > 2:
-    user = argv[2]
-    repo = argv[1]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
-    r = requests.get(url)
-    try:
+if __name__ == "__main__":
+    if len(argv) > 2:
+        user = argv[2]
+        repo = argv[1]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
+
+        r = requests.get(url)
         commits = r.json()
-        for i in range(10):
-            print('{}: {}'.format(commits[i]['sha'],
-                                  commits[i]['commit']['author']['name']))
-    except Exception:
-        pass
+        try:
+            for i in range(10):
+                print("{}: {}".format(
+                    commits[i].get("sha"),
+                    commits[i].get("commit").get("author").get("name")))
+        except IndexError:
+            pass
