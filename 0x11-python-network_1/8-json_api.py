a69da3af91c5872510e089baeea1f80cbaef6521
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-01 19:19:08 +0300

    edited to be main file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,24 +2,24 @@
 """
 requests from an api and searches for a letter
 """
+if __name__ = '__main__':
+    from sys import argv
+    import requests
 
-from sys import argv
-import requests
-
-if len(argv) > 1:
-    val = argv[1]
-else:
-    val = ""
-
-url = 'http://0.0.0.0:5000/search_user'
-params = {'q': val}
-r = requests.post(url, data=params)
-
-try:
-    res = r.json()
-    if res:
-        print('[{}] {}'.format(res['id'], res['name']))
+    if len(argv) > 1:
+        q = argv[1]
     else:
-        print('No result')
-except requests.exceptions.JSONDecodeError:
-    print('Not a valid JSON')
+        q = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+    params = {'q': q}
+    r = requests.post(url, data=params)
+
+    try:
+        res = r.json()
+        if res:
+            print('[{}] {}'.format(res['id'], res['name']))
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
