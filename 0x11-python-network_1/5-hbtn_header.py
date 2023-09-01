7c288859ba933305a8ca26ebb9a088779bde0f26
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-01 17:41:14 +0300

    makes a request and prints the request id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+requests a url and displays values of a header file
+"""
+from sys import argv
+import requests
+
+if len(argv) > 1:
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
