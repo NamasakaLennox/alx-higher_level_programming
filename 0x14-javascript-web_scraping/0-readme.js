eddb7c0dcbfd7225efb5c2f79c4695853e61c72a
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-26 15:51:17 +0300

    added encoding scheme to the script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const fs = require('fs');
 
-fs.readFile(args[2], (err, data) => {
+fs.readFile(args[2], 'utf-8', (err, data) => {
   if (err) throw err;
 
   console.log(data.toString());
