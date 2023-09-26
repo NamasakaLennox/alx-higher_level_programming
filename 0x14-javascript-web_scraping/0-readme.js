619b50c6e24ec231088ace444fd2665ea3161418
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-26 15:46:05 +0300

    reads and prints contents of a file on the console

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* reads contents of a file and displays them */
+const args = process.argv;
+const fs = require('fs');
+
+fs.readFile(args[2], (err, data) => {
+  if (err) throw err;
+
+  console.log(data.toString());
+});
